0b0f2d68e8d0bac3ffefe17f4b35b694dd79b436
kaionobre <kaionobrega00@gmail.com>
2023-08-11 22:52:06 -0300

    Treinando os métodos básicos do Java

--- 01_hello_world/aula1.java
+++ 01_hello_world/aula1.java
@@ -1,6 +1,5 @@
 class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
-
     }
 }
---
+++ ExercicioPablo/Main.java
@@ -0,0 +1,13 @@
+package ExercicioPablo;
+
+public class Main {
+    public static void main(String[] args) {
+        Serie obj1 = new Serie();
+        System.out.println("Minha série preferida é " + obj1.nome );
+        System.out.println(" ");
+        System.out.println("O personagem principal é " + obj1.personagemPrincipal );
+        System.out.println(" ");
+        System.out.println("O número de temporadas são " + obj1.numeroTemporadas );
+
+    }
+}
