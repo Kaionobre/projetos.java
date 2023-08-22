2a8d079ad9504d0a8386cf580da8e603dbf1604b
Kaionobre <kaionobrega00@gmail.com>
2023-08-22 17:50:35 -0300

    algumas mudanças

--- screenmath/src/Principal.java
+++ screenmath/src/Principal.java
@@ -9,10 +9,11 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
 
-
+        //System.out.println(meuFilme.somaDasAvaliacoes);
+        //System.out.println(meuFilme.totalDeAvaliacoes);
+        //System.out.println(meuFilme.pegaMedia());
     }
 }
