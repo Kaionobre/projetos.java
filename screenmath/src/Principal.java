5542f0f7d78cbb44f92adcd1b87fa0eccdc1a67f
Kaionobre <kaionobrega00@gmail.com>
2023-08-25 17:45:14 -0300

    Aprendendo um pouco sobre hierarquia e superclasses

--- screenmath/src/Principal.java
+++ screenmath/src/Principal.java
@@ -1,12 +1,14 @@
+import br.com.alura.screenmath.calculo.CalculadoraDeTempo;
 import br.com.alura.screenmath.modelos.Filme;
+import br.com.alura.screenmath.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Clube da luta";
-        meuFilme.anoDeLancamento =   2002;
-        meuFilme.duracaoEmMinutos = 180;
-
+        meuFilme.setNome("Clube da luta");
+        meuFilme.setAnoDeLancamento(2002);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("O filme tem duração: "+meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -18,5 +20,29 @@ public class Principal {
         //System.out.println(meuFilme.somaDasAvaliacoes);
         //System.out.println(meuFilme.totalDeAvaliacoes);
         //System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodioPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: "+ lost.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("O poderoso chefão");
+        outroFilme.setAnoDeLancamento(1970);
+        outroFilme.setDuracaoEmMinutos(200);
+
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+
+
     }
 }
--- screenmath/src/br/com/alura/screenmath/modelos/Filme.java
+++ screenmath/src/br/com/alura/screenmath/modelos/Filme.java
@@ -1,31 +1,13 @@
 package br.com.alura.screenmath.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+public class Filme extends Titulo {
+    private String diretor;
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
-
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do filme: "+ nome);
-        System.out.println("Ano de lançamento do filme: "+ anoDeLancamento);
-    }
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
